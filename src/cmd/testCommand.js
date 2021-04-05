b92b171d77992d51c18eacf35b1374e5351b5ee9
TheDudeFromCI <thedudefromci@gmail.com>
2021-04-05 11:47:56 -0700

    Fixed argv array modification

--- src/cmd/testCommand.js
+++ src/cmd/testCommand.js
@@ -1,6 +1,6 @@
 import { getDefinedTests, runTests } from '../mineflayerTest.js'
 
-const versions = process.argv.splice(0, 2)
+const versions = process.argv.slice(2)
 if (versions.length === 0) {
   console.log('Usage: npm run mineflayer-test [mcVersion1] [mcVersion2] ...')
   process.exit(1)
